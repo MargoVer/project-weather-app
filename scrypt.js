469c2211253471f0c458508bf6694af38539b961
MargoVer <65725933+MargoVer@users.noreply.github.com>
2020-06-24 08:39:48 +0200

    HTML + CSS + JS

---
+++ scrypt.js
@@ -0,0 +1,75 @@
+let now = new Date();
+let data = document.querySelector("#current-date");
+let date = now.getDate();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thusrday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = days[now.getDay()];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+let year = now.getFullYear();
+data.innerHTML = `${day}, ${month} ${date}, ${year}`;
+
+//get current position - geolocation
+function positionNow(position) {
+  let apiKey = "b1f3a8a2d9fc90849bbd1b29224fc8ef";
+  let lat = position.coords.latitude;
+  let long = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
+
+  axios.get(url).then(showWeather);
+}
+function getCurrentPosition() {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(positionNow);
+}
+let buttonLocation = document.querySelector("#geolocation");
+buttonLocation.addEventListener("click", getCurrentPosition);
+
+//show current weather
+function showWeather(response) {
+  let locationNow = document.querySelector("h1");
+  let h2 = document.querySelector("h2");
+  let tempCelsius = Math.round(response.data.main.temp);
+  let weatherConditions = document.querySelector("#weather-now");
+  locationNow.innerHTML = `${response.data.name}`;
+  h2.innerHTML = `${tempCelsius}°C`;
+
+  weatherConditions.innerHTML = response.data.weather[0].description;
+}
+
+//add a search engine
+function place(event) {
+  event.preventDefault();
+
+  let currentLocation = document.querySelector("#city-input");
+  let apiKey = "b1f3a8a2d9fc90849bbd1b29224fc8ef";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentLocation.value}&appid=${apiKey}&units=metric`;
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${currentLocation.value}`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+let town = document.querySelector("#search-city");
+town.addEventListener("submit", place);
